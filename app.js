23044b4dfbe15027aeeb7f236693b93712d3bb63
NikhilanshSisodia <nikhilanshsisodia@gmail.com>
2024-07-25 15:48:19 +0530

    Adding Functionality to TodoList App

---
+++ app.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#input-box');
+const listContainer = document.querySelector('.list-container');
+
+function addTask(){
+    if(input.value === ''){
+        alert('You must write something!');
+    }else{
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+
+        li.innerHTML = input.value;
+        span.innerHTML = '\u00d7';
+
+        li.appendChild(span);
+        listContainer.appendChild(li);
+        input.value = '';
+    }
+}
+
+listContainer.addEventListener('click', function(e){
+
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('checked');
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
